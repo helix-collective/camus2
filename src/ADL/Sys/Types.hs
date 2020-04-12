1ab56c945135033dbc781baf13ca467b5be85078
paul-thompson-helix <38845656+paul-thompson-helix@users.noreply.github.com>
2020-04-12 15:25:14 +1000

    Update ADL setup (#19)
    Upgrade to current adl version
    
    Use current adlenv setup from jcms
    Get listing of adl stdlib files from ADLC show --adlstdlib
    Add resulting changes from upgraded adl.

--- src/ADL/Sys/Types.hs
+++ src/ADL/Sys/Types.hs
@@ -1,8 +1,9 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-}
 module ADL.Sys.Types(
     Either,
     Error,
     Map,
+    MapEntry(..),
     Maybe,
     Pair,
     Set,
@@ -13,20 +14,45 @@ import Control.Applicative( (<$>), (<*>), (<|>) )
 import Prelude( ($) )
 import qualified Data.Aeson as JS
 import qualified Data.HashMap.Strict as HM
-import qualified Data.Map
+import qualified Data.Map as Map
 import qualified Data.Proxy
-import qualified Data.Set
-import qualified Data.Text
+import qualified Data.Set as Set
+import qualified Data.Text as T
 import qualified Prelude
 
 
-type Either a b = Prelude.Either a b
+type Either = Prelude.Either
 
 
-type Error a  = Prelude.Either Data.Text.Text a
+type Error a  = Prelude.Either T.Text a
 
 
-type Map k v = Data.Map.Map k v
+type Map k v = Map.Map k v
+
+data MapEntry k v = MapEntry
+    { mapEntry_key :: k
+    , mapEntry_value :: v
+    }
+    deriving (Prelude.Eq,Prelude.Ord,Prelude.Show)
+
+mkMapEntry :: k -> v -> MapEntry k v
+mkMapEntry key value = MapEntry key value
+
+instance (AdlValue k, AdlValue v) => AdlValue (MapEntry k v) where
+    atype _ = T.concat
+        [ "sys.types.MapEntry"
+        , "<", atype (Data.Proxy.Proxy :: Data.Proxy.Proxy k)
+        , ",", atype (Data.Proxy.Proxy :: Data.Proxy.Proxy v)
+        , ">" ]
+    
+    jsonGen = genObject
+        [ genField "k" mapEntry_key
+        , genField "v" mapEntry_value
+        ]
+    
+    jsonParser = MapEntry
+        <$> parseField "k"
+        <*> parseField "v"
 
 
 type Maybe = Prelude.Maybe
@@ -35,4 +61,4 @@ type Maybe = Prelude.Maybe
 type Pair a b = (a,b)
 
 
-type Set v = Data.Set.Set v
+type Set v = Set.Set v
